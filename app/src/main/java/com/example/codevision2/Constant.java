1ecb3b27f01698145378ae18765586e8f3edcd43
jeremy viterbo <viterbo1111@gmail.com>
2024-10-06 17:31:27 +0800

    Signed-off-by: jeremy viterbo <viterbo1111@gmail.com> key update

--- app/src/main/java/com/example/codevision2/Constant.java
+++ app/src/main/java/com/example/codevision2/Constant.java
@@ -4,8 +4,8 @@ public class Constant {
     public final static String BASE_URL = "https://api.jdoodle.com/";
     public final static String OCR_API_URL = "https://ocr-extract-text.p.rapidapi.com/";
     public final static String OCR_IMAGE_TEST = "https://qph.cf2.quoracdn.net/main-qimg-60dad75c0dddf8f4aa1a95040d7c3ca5-pjlq";
-    public final static String JDOODLE_SECRET = "1e0cfdcf4e697f17e89e73764728ae850c23ed32dd325c22568c5d4e6a9ee81e";
-    public final static String JDOODLE_CID = "98725b3fa04efb25b7e028789931aa04";
+    public final static String JDOODLE_SECRET = "2b40f59d8d789db0f03de68faa92aed075198e4634fac138e91b2311c408b4d5";
+    public final static String JDOODLE_CID = "c544330134414dd3c9c77b552e002a24";
     public final static String RAPID_API_KEY = "4c20b4aa43msh7817566f25101dbp133e79jsnbfdf89c5fbed";
     public final static int MAX_TIMEOUT = 30;
 
